1159ab6f6ae8a131dda5f30208e4a484a7f9996f
shehriyar-xeverse <sheriyar.a@xeverse.io>
2026-07-21 01:00:13 +0500

    hide send email func using resend

--- src/services/sendOTP.js
+++ src/services/sendOTP.js
@@ -1,32 +1,32 @@
-import { Resend } from "resend";
+// import { Resend } from "resend";
 
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+// const resend = new Resend(process.env.RESEND_API_KEY);
 
 
-export const sendOTP = async (email, otp) => {
-  try {
-    const { data, error } = await resend.emails.send({
-      from: 'onboarding@resend.dev',
-      to: email,
-      subject: "Your Verification Code",
-      html: `<div style="font-family: Arial, sans-serif;">
-          <h2>Email Verification</h2>
-          <p>Your OTP is:</p>
-          <h1 style="letter-spacing:5px;">
-            ${otp}
-          </h1>
-          <p>This OTP will expire in 5 minutes.</p>
-          <p>If you didn't request this, please ignore this email.</p>
-        </div>
-      `,
-    });
-    if (error) {
-      throw new Error(error.message);
-    }
-    return data;
-  } catch (err) {
-    throw err;
-  }
-};
+// export const sendOTP = async (email, otp) => {
+//   try {
+//     const { data, error } = await resend.emails.send({
+//       from: 'onboarding@resend.dev',
+//       to: email,
+//       subject: "Your Verification Code",
+//       html: `<div style="font-family: Arial, sans-serif;">
+//           <h2>Email Verification</h2>
+//           <p>Your OTP is:</p>
+//           <h1 style="letter-spacing:5px;">
+//             ${otp}
+//           </h1>
+//           <p>This OTP will expire in 5 minutes.</p>
+//           <p>If you didn't request this, please ignore this email.</p>
+//         </div>
+//       `,
+//     });
+//     if (error) {
+//       throw new Error(error.message);
+//     }
+//     return data;
+//   } catch (err) {
+//     throw err;
+//   }
+// };
 
